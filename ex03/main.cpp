00c0ccd04cc0c8787cf96774c7e266eacfdb7e71
swautelet <swautele@student.s19.be>
2022-07-13 17:02:52 +0200

    save GG

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -7,6 +7,10 @@ int main()
 	Point c(10, 6);
 	Point point(4.1, 6.1);
 
+	std::cout << "The 3 corners of the triangle are a:" << a.getx() << "," << a.gety();
+	std::cout << "	b:" << b.getx() << "," << b.gety();
+	std::cout << "	c:" << c.getx() << "," << c.gety() << std::endl;
+	std::cout << "The point searched is :" << point.getx() << "," << point.gety() << std::endl;
 	if (bsp(a, b, c, point))
 		std::cout << "point is in the triangle." << std::endl;
 	else
